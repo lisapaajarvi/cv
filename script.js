a5f3a716eb8fc7e7fd16b0a40dea6982e77fb831
Lisa Pääjärvi <lisapaajarvi@hotmail.com>
2020-11-11 16:33:33 +0100

    added functions

--- script.js
+++ script.js
@@ -1,21 +1,49 @@
-window.onload = main;
+window.addEventListener("load", main);
 
 function main() {
     addEventListeners();
 }
 
 function addEventListeners() {
-    let leafDiv2 = document.querySelector(".div2")
-    leafDiv2.onclick = openLeafDiv2;
+let contactIcon = document.getElementById("contact-icon")
+contactIcon.onclick = openContactPage;
+let aboutIcon = document.getElementById("about-icon")
+aboutIcon.onclick = openAboutPage;
+
+    //    let leafDiv2 = document.querySelector(".div2")
+//    leafDiv2.onclick = openLeafDiv2;
 
  //   let leafDiv4 = document.getElementsByClassName("div4");
  //   leafDiv4.onclick = openLeafDiv4;
 }
 
+function openContactPage() {
+    removeCurrentPage();
+    let contactPage = document.getElementById("contact");
+    contactPage.classList.add("show-textdiv");
+}
+
+function openAboutPage() {
+    removeCurrentPage();
+    let aboutPage = document.getElementById("about");
+    aboutPage.classList.add("show-textdiv");
+}
+
+function removeCurrentPage() {
+    const pages = document.querySelectorAll(".textdiv");
+    for (let i = 0; i < pages.length; i++) {
+        if (pages[i].classList.contains("show-textdiv")) {
+            pages[i].classList.remove("show-textdiv");
+            break;
+        }
+    }      
+}
+
+/*
 function openLeafDiv2(event) {
     let leafDiv2 = event.target
     leafDiv2.style.opacity = 1;
-    leafDiv2.style.borderRadius = "10rem 0 10rem 0"
+    leafDiv2.style.borderRadius = "8rem 0 8rem 0"
     let textDiv1 = document.querySelector(".div3")
     textDiv1.style.visibility = "visible";
-}
+} */
